f3c1c61136f091cfd3f48f745e24d9532f7ee03b
CUHP_VN <phuc260706@gmail.com>
2025-04-21 14:28:13 +0700

    revice

--- Assets/Script/UI/Revive.cs
+++ Assets/Script/UI/Revive.cs
@@ -1,10 +1,20 @@
+using TMPro;
 using UnityEngine;
 
 public class Revive : MonoBehaviour
 {
+	public TMP_Text text;
 	public GameObject DeadScene;
-	public void Yes()
+    private void OnEnable()
+    {
+        SaveSystem.Save();
+        text.text = $"{(int)(UIManager.Instance.GetScore() / 10f)}";
+    }
+    public void Yes()
 	{
+		int req = (int)(UIManager.Instance.GetScore() / 10f);
+        if (CoinManager.Instance.GetCoin() < req) return;
+		CoinManager.Instance.RemoveCoin(req);
 		MapSpawner.Instance.SetBase();
 		PowerUp.Instance.BreakShieldPowerUp();
 		StateManager.Instance.ChangeState(StateManager.Instance.GetLateState());
